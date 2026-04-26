2c077abba5f23134059dc684357f3e14610a7eac
Leigh West <leighwest@hotmail.com>
2026-04-26 14:28:14 +1000

    Removed SSH key pair from Terraform management

--- key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "keypair" {
-  key_name   = "orders"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
-}
--- vars.tf
+++ vars.tf
@@ -2,13 +2,6 @@ variable "AWS_REGION" {
   default = "ap-southeast-4"
 }
 variable "PERSONAL_IP_ADDRESS" {}
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "orders"
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "orders.pub"
-}
 
 variable "PATH_TO_EC2_STOP_SCRIPT" {
   default = "scripts/ec2_stop.py"
