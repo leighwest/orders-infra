b67ac043b07e4585360eb63be55debdee81dc123
Leigh West <leighwest@hotmail.com>
2026-04-27 10:04:24 +1000

    Open up port 22 for github actions

--- securitygroup.tf
+++ securitygroup.tf
@@ -6,12 +6,12 @@ resource "aws_security_group" "default" {
   vpc_id      = data.aws_vpc.default.id
   description = "default VPC security group"
 
-  // SSH access from a specific IP address
+  // SSH access open for GitHub Actions
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.PERSONAL_IP_ADDRESS]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   // HTTP access from anywhere
