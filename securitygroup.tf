404899113e0cd0817883b46e55f460f50fc8a438
Leigh West <leighwest@hotmail.com>
2026-04-27 10:37:06 +1000

    Added egress rule to security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -37,4 +37,12 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  // Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
