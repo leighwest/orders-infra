88a8a857a560023e2b2f65324794fc742c8905a0
Leigh West <leighwest@hotmail.com>
2026-04-29 08:10:32 +1000

    Close access to port 8080

--- securitygroup.tf
+++ securitygroup.tf
@@ -30,14 +30,6 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  // Custom TCP access on port 8080 from anywhere
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   // Allow all outbound traffic
   egress {
     from_port   = 0
