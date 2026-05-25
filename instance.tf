22124e89304db0b2d9b9a9cc6f844529b189f6c6
Leigh West <leighwest@hotmail.com>
2026-05-25 15:48:11 +1000

    Remove elastic IP

--- instance.tf
+++ instance.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "orders" {
-  ami           = "ami-0fddd1724e3df67ed"
-  instance_type = "t3.small"
+  ami                         = "ami-0fddd1724e3df67ed"
+  instance_type               = "t3.small"
+  associate_public_ip_address = true
+
   tags = {
     Name = "orders-server"
   }
@@ -9,8 +11,4 @@ resource "aws_instance" "orders" {
   user_data              = data.cloudinit_config.instance-bootstrap.rendered
   key_name               = "orders"
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
-}
-
-resource "aws_eip" "orders-eip" {
-  instance = aws_instance.orders.id
 }
