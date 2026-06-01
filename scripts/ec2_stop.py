ed0478a0933c5f781be190432b82efad32625045
Leigh West <leighwest@hotmail.com>
2026-06-01 14:20:32 +1000

    Fix: filter for running instances only in stop Lambda

--- scripts/ec2_stop.py
+++ scripts/ec2_stop.py
@@ -8,13 +8,14 @@ def lambda_handler(event, context):
     response = ec2.describe_instances(
         Filters=[
             {'Name': 'tag:Name', 'Values': ['orders-server']}
+            {'Name': 'instance-state-name', 'Values': ['running']}
         ]
     )
 
     instances = response['Reservations']
     if not instances:
-        raise Exception('No matching instance found')
-
+        raise Exception('No running instance found with tag Name=orders-server')
+        
     instance_id = instances[0]['Instances'][0]['InstanceId']
     ec2.stop_instances(InstanceIds=[instance_id])
     print('Stopped instance: ' + instance_id)
