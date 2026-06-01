2d351ad6a71b04bddd50d5a00c2dd969320c212c
Leigh West <leighwest@hotmail.com>
2026-06-01 15:08:41 +1000

    Fix: ignore associate_public_ip_address drift to prevent instance replacement when stopped

--- instance.tf
+++ instance.tf
@@ -11,4 +11,8 @@ resource "aws_instance" "orders" {
   user_data              = data.cloudinit_config.instance-bootstrap.rendered
   key_name               = "orders"
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+
+  lifecycle {
+    ignore_changes = [associate_public_ip_address]
+  }
 }
