019738d301229ee1ff4fa5e76451212da76ee9c5
Leigh West <leighwest@hotmail.com>
2026-06-01 14:23:13 +1000

    Fix: missing comma

--- scripts/ec2_stop.py
+++ scripts/ec2_stop.py
@@ -7,7 +7,7 @@ ec2 = boto3.client('ec2', region_name=region)
 def lambda_handler(event, context):
     response = ec2.describe_instances(
         Filters=[
-            {'Name': 'tag:Name', 'Values': ['orders-server']}
+            {'Name': 'tag:Name', 'Values': ['orders-server']},
             {'Name': 'instance-state-name', 'Values': ['running']}
         ]
     )
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
     instances = response['Reservations']
     if not instances:
         raise Exception('No running instance found with tag Name=orders-server')
-        
+
     instance_id = instances[0]['Instances'][0]['InstanceId']
     ec2.stop_instances(InstanceIds=[instance_id])
     print('Stopped instance: ' + instance_id)
