38b44e86259ae1ef9c330ae436c772fa9c174c12
Leigh West <leighwest@hotmail.com>
2026-06-05 09:04:24 +1000

    Make cupcake images bucket public

--- s3.tf
+++ s3.tf
@@ -2,6 +2,32 @@ resource "aws_s3_bucket" "orders" {
   bucket = "cupcake-orders-images"
 }
 
+resource "aws_s3_bucket_public_access_block" "orders" {
+  bucket = aws_s3_bucket.orders.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "orders" {
+  bucket     = aws_s3_bucket.orders.id
+  depends_on = [aws_s3_bucket_public_access_block.orders]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.orders.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket" "deploy" {
   bucket = "orders-deploy-artefacts"
 }
