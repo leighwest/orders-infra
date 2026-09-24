5da70a1c19e902bc67c9bc42a91b61c7017ace7c
Leigh West <leighwest@hotmail.com>
2026-09-25 07:58:54 +1000

    Add thumbprint list to oidc provider

--- iam.tf
+++ iam.tf
@@ -34,6 +34,10 @@ resource "aws_ssm_parameter" "smtp_password" {
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+  ]
 }
 
 resource "aws_iam_role" "oidc_role" {
